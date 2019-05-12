5f51e2c3410ed315dc3aae6247ed390f7ce3df18
kuzandro <kuzandro@gmail.com>
2019-05-12 23:00:12 +0300

    Easy UI

---
+++ UI/workspace/build/arttaxi/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kuzandro/DuckieTown/gym-duckietown/gym_duckietown/workspace/src/arttaxi
+# Build directory: /home/kuzandro/DuckieTown/gym-duckietown/gym_duckietown/workspace/build/arttaxi
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ UI/workspace/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/kuzandro/DuckieTown/gym-duckietown/gym_duckietown/workspace/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/kuzandro/DuckieTown/gym-duckietown/gym_duckietown/workspace/devel/lib:$LD_LIBRARY_PATH"
+export PWD="/home/kuzandro/DuckieTown/gym-duckietown/gym_duckietown/workspace/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/kuzandro/DuckieTown/gym-duckietown/gym_duckietown/workspace/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/kuzandro/DuckieTown/gym-duckietown/gym_duckietown/workspace/src:$ROS_PACKAGE_PATH"
